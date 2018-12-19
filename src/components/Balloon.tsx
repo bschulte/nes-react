09d0835f6c43c6fa3d5109a531d85ba82cb3d403
Brian Schulte <bschulte@Brians-MacBook-Pro.local>
2018-12-18 20:20:34 -0500

    added balloon

---
+++ src/components/Balloon.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import classNames from "classnames";
+
+export interface BalloonProps {
+  fromLeft: boolean;
+  fromRight: boolean;
+  style: any;
+}
+
+export default class Balloon extends React.Component<BalloonProps, any> {
+  public render() {
+    const { fromLeft, fromRight, children, style } = this.props;
+    return (
+      <div
+        className={classNames("nes-balloon", {
+          "from-left": fromLeft,
+          "from-right": fromRight
+        })}
+        style={style}
+      >
+        {children}
+      </div>
+    );
+  }
+}
