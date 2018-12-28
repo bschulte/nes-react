dcab59c5170f7cc40f99de4ed8f69940560deca7
Adrien Boutigny <adrien.boutigny@gmail.com>
2018-12-28 17:23:56 +0100

    fix(Progress): convert to SFC and pass down all the props

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -10,33 +10,22 @@ export interface ProgressProps {
   warning?: boolean;
   error?: boolean;
   pattern?: boolean;
+  className?: string
 }
 
-export default class Progress extends React.Component<ProgressProps, any> {
-  public render() {
-    const {
-      style,
-      value,
-      max,
-      primary,
-      success,
-      warning,
-      error,
-      pattern
-    } = this.props;
-    return (
-      <progress
-        style={style}
-        value={value}
-        max={max}
-        className={classNames("nes-progress", {
-          "is-primary": primary,
-          "is-success": success,
-          "is-warning": warning,
-          "is-error": error,
-          "is-pattern": pattern
-        })}
-      />
-    );
-  }
-}
+const Progress: React.SFC<ProgressProps> = ({ className, value, max, primary, success, warning, error, pattern, ...other }) => (
+  <progress
+    value={value}
+    max={max}
+    className={classNames(className, "nes-progress", {
+      "is-primary": primary,
+      "is-success": success,
+      "is-warning": warning,
+      "is-error": error,
+      "is-pattern": pattern
+    })}
+    {...other}
+  />
+);
+
+export default Progress;
