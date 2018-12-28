489b94529b931cc13984923f894370d65e0fe976
Adrien Boutigny <adrien.boutigny@gmail.com>
2018-12-28 17:16:41 +0100

    fix(Icon): convert to SFC and pass down all the props

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -25,34 +25,22 @@ export interface IconProps {
   empty?: boolean;
   transparent?: boolean;
   half?: boolean;
-  style?: any;
+  className?: string;
 }
 
-export default class Icon extends React.Component<IconProps, any> {
-  public render() {
-    const {
-      style,
-      icon,
-      small,
-      medium,
-      large,
-      empty,
-      transparent,
-      half
-    } = this.props;
-    return (
-      <i
-        className={classNames("nes-icon", icon, {
-          "is-small": small,
-          "is-medium": medium,
-          "is-large": large,
-          // These two seem better named to be switched to me
-          "is-transparent": empty,
-          "is-empty": transparent,
-          "is-half": half
-        })}
-        style={style}
-      />
-    );
-  }
-}
+const Icon: React.SFC<IconProps> = ({ className, icon, small, medium, large, empty, transparent, half, ...other }) => (
+  <i
+    className={classNames(className, "nes-icon", icon, {
+      "is-small": small,
+      "is-medium": medium,
+      "is-large": large,
+      // These two seem better named to be switched to me
+      "is-transparent": empty,
+      "is-empty": transparent,
+      "is-half": half
+    })}
+    {...other}
+  />
+);
+
+export default Icon;
